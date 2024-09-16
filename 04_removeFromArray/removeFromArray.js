539e1c9a0dac5b88a80e9e2062f7204dd0ad9ab9
Gabriel <gabrielpachecotec@gmail.com>
2024-09-16 19:49:08 +0100

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedNums) {
+    return array.filter(num => {
+        if (removedNums.includes(num)) {
+            return false
+        }
+        return true;
+    });
 };
 
+console.log(removeFromArray([1, 2, 3], 2, 3));
 // Do not edit below this line
 module.exports = removeFromArray;
