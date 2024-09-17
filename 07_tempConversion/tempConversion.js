cd935e68cb4c39f6dd94ae1dc7f5bd47e8c3e4c6
Gabriel <gabrielpachecotec@gmail.com>
2024-09-17 07:20:43 +0100

    Finish 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+  const celsius = (fahrTemp - 32) * (5 / 9);
+  return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const fahrenheit = celsiusTemp * (9 / 5) + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
