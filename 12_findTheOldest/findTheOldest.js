7542f3940857b9bf9cec6efa4487550f486fcce0
Gabriel <gabrielpachecotec@gmail.com>
2024-09-20 07:27:21 +0100

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,46 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentYear = new Date().getFullYear();
+    let age;
+    const oldestAge = peopleArray
+    .reduce((previous, next) => { 
+        if (!next.yearOfDeath) {
+            age = currentYear - next.yearOfBirth;
+        } else {
+            age = next.yearOfDeath - next.yearOfBirth;
+        }
+        return previous > age ? previous : age;
+    }, 0);
 
+    return peopleArray.filter(person => {
+        if (!person.yearOfDeath) {
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (age === oldestAge) {
+            return true;
+        }
+    })[0];
 };
+
+console.log(findTheOldest([
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]));
 
 // Do not edit below this line
 module.exports = findTheOldest;
