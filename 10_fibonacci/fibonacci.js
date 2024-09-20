077ae2715ba7526a9c188baa9e4be1df15dc6498
Gabriel <gabrielpachecotec@gmail.com>
2024-09-20 06:48:43 +0100

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let position = +num;
+    let before = 0;
+    let next = 0;
 
+    if (position < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 0; i < position; i++) {
+            if (i === 0) {
+                next = 1;
+                continue;
+            }
+            const placeholder = next;
+            next += before;
+            before = placeholder;
+        }
+    }
+    return next;
 };
+
+console.log(fibonacci(25));
 
 // Do not edit below this line
 module.exports = fibonacci;
