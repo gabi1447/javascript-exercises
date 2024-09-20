f6d41fa80b5aabbe771521f24c8a35f1a9d00322
Gabriel <gabrielpachecotec@gmail.com>
2024-09-20 06:24:34 +0100

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordAlpha = removeNonAlphaChars(word);
 
+    let reversedWord = reverseString(wordAlpha);
+    return wordAlpha === reversedWord ? true : false;
 };
+
+function isAlphaNum(char) {
+    const pattern = /^[A-Z0-9]{1}$/i;
+    return pattern.test(char);
+}
+
+function reverseString(word) {
+    let reversed = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversed += word[i];
+    }
+    return reversed;
+}
+
+function removeNonAlphaChars(word) {
+    return word
+    .split('')
+    .filter(char => {
+        return isAlphaNum(char) ? true : false;
+    })
+    .join('')
+    .toLowerCase();
+}
+
+palindromes("ZZZZ car, a man, a maracaz.");
 
 // Do not edit below this line
 module.exports = palindromes;
